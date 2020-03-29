4c1a85f9f2e842de50acd76e6e263783e64d3631
sozinho <56240097+sozinhoL@users.noreply.github.com>
2020-03-29 16:17:13 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -6,48 +6,15 @@ const incidentController = require('./controller/incidentController');
 const profileController = require('./controller/profileController');
 const sessionController = require('./controller/sessionController');
 
-routes.post('/login', celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    id: Joi.string().required()
-  })
-}), sessionController.index)
+routes.post('/login',  sessionController.index)
 
 routes.get('/ongs', ongController.index)
-routes.post('/ongs', celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    name: Joi.string().required(),
-    email: Joi.string().required().email(),
-    whatsapp: Joi.string().required().min(10).max(11),
-    city: Joi.string().required(),
-    uf: Joi.string().required().length(2)
-  })
-}), ongController.create)
+routes.post('/ongs', ongController.create)
 
-routes.get('/profile', celebrate({
-  [Segments.HEADERS]: Joi.object({
-    authorization: Joi.string().required(),
-  }).unknown()
-}), profileController.index)
+routes.get('/profile', profileController.index)
 
-routes.get('/incident', celebrate({
-  [Segments.QUERY]: Joi.object().keys({
-    page: Joi.number()
-  })
-}), incidentController.index)
-routes.post('/incident', celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    title: Joi.string().required(),
-    desciption: Joi.string().required(),
-    value: Joi.number().required(),
-  }),
-  [Segments.HEADERS]: Joi.object({
-      authorization: Joi.string().required(),
-    }).unknown()
-}), incidentController.create)
-routes.delete('/incident/:id', celebrate({
-  [Segments.PARAMS]: Joi.object().keys({
-    id: Joi.number().required()
-  })
-}), incidentController.delete)
+routes.get('/incident', incidentController.index)
+routes.post('/incident', incidentController.create)
+routes.delete('/incident/:id', incidentController.delete)
 
 module.exports = routes;
